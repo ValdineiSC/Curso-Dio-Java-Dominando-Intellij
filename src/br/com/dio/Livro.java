1892f51e49336db5f2c27ff48520396312b70d70
ValdineiSC <valdineichaves@gmail.com>
2022-01-28 15:37:39 -0300

    Criação da class Livro.

---
+++ src/br/com/dio/Livro.java
@@ -0,0 +1,6 @@
+package br.com.dio;
+
+public class Livro {
+    public Livro(Object nome) {
+    }
+}
--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -9,7 +9,42 @@ public class PrimeiroProgramaJava {
         System.out.println("Hello World!" + (a+b));*/
         Gato gato = new Gato();
         System.out.println(gato);
+        livro Livro = new livro("O problema dos 3 corpos", 300);
+        System.out.println(Livro);
 
 
+    }
+}
+class livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
