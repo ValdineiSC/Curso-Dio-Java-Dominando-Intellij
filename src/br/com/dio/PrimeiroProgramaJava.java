9457643fb071c92e521e51d6e349cf0fc9171de1
ValdineiSC <valdineichaves@gmail.com>
2022-01-28 14:58:02 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 10;
+        System.out.println("Hello World!" + (a+b));*/
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+    }
+}
